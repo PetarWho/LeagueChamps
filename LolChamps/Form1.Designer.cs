956239c237942c11e48906c8f7873b512f59d453
PetarWho <petarpenev02@gmail.com>
2022-01-29 05:17:11 +0200

    Update
    v1

---
+++ LolChamps/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace LolChamps
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.champListBox = new System.Windows.Forms.ListBox();
+            this.RemoveBtn = new System.Windows.Forms.Button();
+            this.AddBtn = new System.Windows.Forms.Button();
+            this.champNameBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.ClearListBtn = new System.Windows.Forms.Button();
+            this.champCountLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // champListBox
+            // 
+            this.champListBox.BackColor = System.Drawing.Color.White;
+            this.champListBox.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.champListBox.FormattingEnabled = true;
+            this.champListBox.ItemHeight = 20;
+            this.champListBox.Location = new System.Drawing.Point(12, 24);
+            this.champListBox.Name = "champListBox";
+            this.champListBox.Size = new System.Drawing.Size(178, 284);
+            this.champListBox.Sorted = true;
+            this.champListBox.TabIndex = 0;
+            // 
+            // RemoveBtn
+            // 
+            this.RemoveBtn.BackColor = System.Drawing.Color.Silver;
+            this.RemoveBtn.Font = new System.Drawing.Font("Comic Sans MS", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.RemoveBtn.Location = new System.Drawing.Point(249, 196);
+            this.RemoveBtn.Name = "RemoveBtn";
+            this.RemoveBtn.Size = new System.Drawing.Size(148, 50);
+            this.RemoveBtn.TabIndex = 1;
+            this.RemoveBtn.Text = "Remove";
+            this.RemoveBtn.UseVisualStyleBackColor = false;
+            this.RemoveBtn.Click += new System.EventHandler(this.RemoveBtn_Click);
+            // 
+            // AddBtn
+            // 
+            this.AddBtn.BackColor = System.Drawing.Color.Silver;
+            this.AddBtn.Font = new System.Drawing.Font("Comic Sans MS", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AddBtn.Location = new System.Drawing.Point(249, 140);
+            this.AddBtn.Name = "AddBtn";
+            this.AddBtn.Size = new System.Drawing.Size(148, 50);
+            this.AddBtn.TabIndex = 1;
+            this.AddBtn.Text = "Add New";
+            this.AddBtn.UseVisualStyleBackColor = false;
+            this.AddBtn.Click += new System.EventHandler(this.AddBtn_Click);
+            // 
+            // champNameBox
+            // 
+            this.champNameBox.Location = new System.Drawing.Point(249, 112);
+            this.champNameBox.Name = "champNameBox";
+            this.champNameBox.Size = new System.Drawing.Size(148, 22);
+            this.champNameBox.TabIndex = 2;
+            this.champNameBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.champNameBox_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(249, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 26);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Champion name";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(220, 249);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(212, 80);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "To remove item, select one from the list or type a name and click on Remove";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // ClearListBtn
+            // 
+            this.ClearListBtn.BackColor = System.Drawing.Color.Silver;
+            this.ClearListBtn.Font = new System.Drawing.Font("Comic Sans MS", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ClearListBtn.Location = new System.Drawing.Point(249, 10);
+            this.ClearListBtn.Name = "ClearListBtn";
+            this.ClearListBtn.Size = new System.Drawing.Size(148, 45);
+            this.ClearListBtn.TabIndex = 5;
+            this.ClearListBtn.Text = "Clear List";
+            this.ClearListBtn.UseVisualStyleBackColor = false;
+            this.ClearListBtn.Click += new System.EventHandler(this.ClearListBtn_Click);
+            // 
+            // champCountLabel
+            // 
+            this.champCountLabel.BackColor = System.Drawing.Color.Transparent;
+            this.champCountLabel.Font = new System.Drawing.Font("Comic Sans MS", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.champCountLabel.Location = new System.Drawing.Point(142, 2);
+            this.champCountLabel.Name = "champCountLabel";
+            this.champCountLabel.Size = new System.Drawing.Size(48, 20);
+            this.champCountLabel.TabIndex = 6;
+            this.champCountLabel.Text = "0";
+            this.champCountLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 2);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Names in the list:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(432, 323);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.champCountLabel);
+            this.Controls.Add(this.ClearListBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.champNameBox);
+            this.Controls.Add(this.AddBtn);
+            this.Controls.Add(this.RemoveBtn);
+            this.Controls.Add(this.champListBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(450, 370);
+            this.MinimumSize = new System.Drawing.Size(450, 370);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LOL Champs";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox champListBox;
+        private System.Windows.Forms.Button RemoveBtn;
+        private System.Windows.Forms.Button AddBtn;
+        private System.Windows.Forms.TextBox champNameBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button ClearListBtn;
+        private System.Windows.Forms.Label champCountLabel;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ LolChamps/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LolChamps // This project was originally created for League of Legends, that's why the names looks like this :D
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            fileDirectory();
+        }
+        public int ChampCount
+        {
+            get => int.Parse(champCountLabel.Text);
+            set => champCountLabel.Text = value.ToString();
+        }
+        private string filePath = @"champList.txt";
+        private StreamReader sr;
+        private void fileDirectory()
+        {
+            if (!File.Exists(@"..\champList.txt"))
+            {
+                File.Create(@"..\champList.txt").Close();
+            }
+            else
+            {
+                champListBox.Items.Clear();
+                try
+                {
+                    sr = new StreamReader(filePath);
+                    while (!sr.EndOfStream)
+                    {
+                        champListBox.Items.Add(sr.ReadLine());
+                    }
+                    sr.Dispose();
+                }
+                catch (Exception) { }
+                finally { ChampCount = champListBox.Items.Count; }
+            }
+        }
+        private void AddChamp()
+        {
+            string currentChamp = champNameBox.Text.ToLower().Trim();
+            if (currentChamp == "")
+            {
+                MessageBox.Show("Name cannot be empty", "Oops..", MessageBoxButtons.OK);
+                return;
+            }
+            currentChamp = FixName(currentChamp);
+            if (!champListBox.Items.Contains(currentChamp))
+            {
+                champListBox.Items.Add(currentChamp);
+                StreamWriter sw = File.AppendText(filePath);
+                ChampCount++;
+                sw.WriteLine(currentChamp);
+                sw.Dispose();
+            }
+            else
+            {
+                MessageBox.Show("This name is already on the list!", "Already on the list!", MessageBoxButtons.OK);
+            }
+            champNameBox.Clear();
+        }
+        private void AddBtn_Click(object sender, EventArgs e)
+        {
+            AddChamp();
+        }
+
+        private void ClearListBtn_Click(object sender, EventArgs e)
+        {
+            champListBox.Items.Clear();
+            ChampCount = 0;
+        }
+        static string FixName(string str)
+        {
+            str = str.Trim();
+            char[] letters = str.ToCharArray();
+            letters[0] = char.ToUpper(letters[0]);
+            string result = string.Empty;
+            bool upTheNextOne = false;
+            foreach (var letter in letters)
+            {
+                if (upTheNextOne)
+                {
+                    char current = char.ToUpper(letter);
+                    result += current;
+                    upTheNextOne = false;
+                    continue;
+                }
+                if (letter == ' ' || letter == '\'')
+                {
+                    upTheNextOne = true;
+                }
+
+                result += letter;
+            }
+            return result;
+        }
+
+        private void champNameBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                AddChamp();
+                e.Handled = e.SuppressKeyPress = true;
+            }
+        }
+
+        private void RemoveBtn_Click(object sender, EventArgs e)
+        {
+            bool hasInput = false;
+            string currentChamp = champNameBox.Text.Trim();
+            if (currentChamp != "")
+            {
+                currentChamp = FixName(currentChamp);
+                if (champListBox.Items.Count > 0)
+                {
+                    if (champListBox.Items.Contains(currentChamp))
+                    {
+                        hasInput = true;
+                        champListBox.Items.Remove(currentChamp);
+                        ChampCount--;
+                    }
+                }
+                champNameBox.Text = "";
+            }
+            if (!hasInput)
+            {
+                if (champListBox.SelectedIndex == -1)
+                    MessageBox.Show("Please select a champ or type it's name!", "No item selected!", MessageBoxButtons.OK);
+                else
+                {
+                    champListBox.Items.Remove(champListBox.SelectedItem);
+                    ChampCount--;
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            StreamWriter sw = new StreamWriter(filePath);
+            foreach (var item in champListBox.Items)
+            {
+                sw.WriteLine(item);
+            }
+            sw.Dispose();
+        }
+    }
+}
